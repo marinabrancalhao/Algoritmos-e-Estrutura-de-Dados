18bd33aae441e6e8f130a1208a6cad7fbd43ca92
Marina <mahbrancalhao@gmail.com>
2025-11-03 00:31:19 -0300

    objetos parte II

--- Unidade 1/Aula 4/Objetos.java
+++ Unidade 1/Aula 4/Objetos.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Objetos{
     //Atributos - características do objeto
     private String nome;
@@ -18,12 +20,64 @@ public class Objetos{
         System.out.println("Peso: " + peso);
     }
 
+     public void aniversario(){
+        this.idade += 1;
+    }
+
+    //Gets e Sets
+    public String getNome(){
+        return this.nome;
+    } //precisa retornar alguma informação
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    } //precisa retornar alguma informação
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+      public float getPeso(){
+        return this.peso;
+    } //precisa retornar alguma informação
+
+    public void setPeso(float peso){
+        this.peso = peso;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Objetos pessoa = new Objetos("Marina", 30, 90.5f);
         Objetos pessoa2 = new Objetos("Bruno", 35, 72.5f);
 
         pessoa.exibirInfos();
         pessoa2.exibirInfos();
+        pessoa.aniversario();
+        pessoa.exibirInfos();
 
+        pessoa.setNome("Marina Brancalhão");
+        pessoa.exibirInfos();
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine(); 
+        
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.print("Digite seu peso: ");
+        float peso = scanner.nextFloat();
+        scanner.nextLine();
+
+        Objetos pessoa3 = new Objetos(nome, idade, peso);
+
+        pessoa3.exibirInfos();
+
+        scanner.close();
     }
 }
