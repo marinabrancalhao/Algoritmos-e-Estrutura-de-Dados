3d6a63641442a7b2cff3ba173cf0135527145c5b
Marina <mahbrancalhao@gmail.com>
2025-11-04 16:04:16 -0300

    exercícios 2

---
+++ Unidade 1/Exercicios/ExercicioDois.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ExercicioDois {
+/*Exercício 2: Verificador de Características de Um Produto
+
+Em uma loja, os produtos têm códigos específicos. 
+Escreva um programa que, ao ler o código de um produto, indique se o número é par ou ímpar. 
+Imprima o resultado na tela para ajudar o setor de inventário a identificar características básicas dos produtos. */
+
+private int codigoProduto;
+
+public ExercicioDois(int codigoProduto){
+    this.codigoProduto = codigoProduto;
+}
+
+public void verificarParOuImpar(){
+    if (this.codigoProduto % 2 == 0) {
+        System.out.println("O código é um número par");  
+    } else {
+        System.out.println("O código é um número ímpar");
+    }
+}
+
+public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("Digite o código do produto: ");
+    int codigoProduto = scanner.nextInt();
+
+    ExercicioDois codigo = new ExercicioDois(codigoProduto);
+    
+    codigo.verificarParOuImpar();
+
+    scanner.close(); 
+}
+}
