c220a30d8b68a52b2085bfd2a98c281319833f6f
Marina <mahbrancalhao@gmail.com>
2025-11-06 15:26:41 -0300

    números primos

---
+++ Unidade 1/Atividade/Exercicio6.java
@@ -0,0 +1,29 @@
+/*
+Exercício 6: Seleção de Números Primos para Criptografia
+
+Em um sistema de segurança, números primos são utilizados como base para a criptografia.
+Escreva um programa que encontre e exiba todos os números primos entre 1 e 100, ajudando a equipe de segurança a selecionar números para esse propósito. 
+*/
+public class Exercicio6 {
+    public static void main(String[] args) {
+
+        System.out.println("== Números Primos 0 a 100 ==");
+
+        for(int num = 1; num <= 100; num++){
+            int divisores = 0;
+
+            for(int i = 1; i <= num; i++){
+                if (num % i == 0) {
+                    divisores++;
+                }
+            }
+
+            if (divisores == 2) {
+                System.out.println(num);
+            }
+            
+        }
+        
+    }
+    
+}
