c28e2bc2c9e86e89406d3f8f2f0882833b6f01e6
Marina <mahbrancalhao@gmail.com>
2025-11-07 00:19:18 -0300

    gerenciamento de estoque

---
+++ Unidade 1/Atividade/Exercicio10.java
@@ -0,0 +1,11 @@
+/*
+Exercício 10: Desafio Final - Sistema de Gerenciamento de Estoque para uma Loja
+
+Desenvolva um sistema completo de gerenciamento de estoque para uma loja. 
+Permita que o usuário adicione, remova e atualize itens no estoque, usando classes para representar os produtos e listas para organizar os itens cadastrados. 
+Esse sistema ajudará na administração dos produtos disponíveis para venda.
+*/
+
+public class Exercicio10 {
+    
+}
