c4246d168587491cd821672ec3e5798c396426ad
Marina <mahbrancalhao@gmail.com>
2025-11-07 00:18:30 -0300

    cadastro de clientes

---
+++ Unidade 1/Atividade/Exercicio9.java
@@ -0,0 +1,10 @@
+/*
+Exercício 9: Gerenciando um Cadastro de Clientes
+
+Em uma agência de viagens, você precisa organizar o cadastro dos clientes. 
+Crie uma classe Pessoa com atributos como nome, idade e endereço, e desenvolva um programa que permita criar e gerenciar uma lista desses clientes.
+*/
+
+public class Exercicio9 {
+    
+}
