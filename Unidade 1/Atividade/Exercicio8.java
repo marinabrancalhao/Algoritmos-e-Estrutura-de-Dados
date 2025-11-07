d0481ed546aa82a3d5094bdfa8e1501a66352fdd
Marina <mahbrancalhao@gmail.com>
2025-11-07 00:17:56 -0300

    análise de frequência

---
+++ Unidade 1/Atividade/Exercicio8.java
@@ -0,0 +1,10 @@
+/*
+Exercício 8: Análise de Frequência de Pedidos no Delivery
+
+Em uma análise dos pedidos feitos por um cliente, você precisa descobrir qual item é mais pedido. 
+Crie um programa que solicite uma lista de números (representando o código dos pedidos) e exiba o item que mais se repete na lista.
+*/
+
+public class Exercicio8 {
+    
+}
