3c0353edb78ea134b31b17f85a4bb44810619859
Marina <mahbrancalhao@gmail.com>
2025-11-07 00:17:16 -0300

    jogo da velha

---
+++ Unidade 1/Atividade/Exercicio7.java
@@ -0,0 +1,10 @@
+/*
+Exercício 7: Jogo da Velha Interativo
+
+Desenvolva um programa de Jogo da Velha (Tic-Tac-Toe) para uma brincadeira entre amigos. 
+Utilize uma matriz para representar o tabuleiro e permita que dois jogadores façam jogadas alternadas, verificando quem será o vencedor!
+*/
+
+public class Exercicio7 {
+    
+}
