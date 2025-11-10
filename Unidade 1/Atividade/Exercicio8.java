f20008d5c505cfb80f8d590e8a165539b73bc8d6
Marina <mahbrancalhao@gmail.com>
2025-11-09 22:52:41 -0300

    frequência de pedidos

--- Unidade 1/Atividade/Exercicio8.java
+++ Unidade 1/Atividade/Exercicio8.java
@@ -5,6 +5,50 @@ Em uma análise dos pedidos feitos por um cliente, você precisa descobrir qual 
 Crie um programa que solicite uma lista de números (representando o código dos pedidos) e exiba o item que mais se repete na lista.
 */
 
+import java.util.Scanner;
+
 public class Exercicio8 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos códigos de pedidos você deseja analisar? ");
+        int quantidade = scanner.nextInt();
+
+        int[] codigosPedidos = new int[quantidade];
+
+        for(int i = 0; i < codigosPedidos.length; i++){
+            System.out.print("Código #" + (i + 1)+ ": ");
+            codigosPedidos[i] = scanner.nextInt();
+        }
+
+        int itemMaisFrequente = 0;
+        int contagemMaisAlta = 0;
+
+        for(int i = 0; i < codigosPedidos.length; i++) {
+            int itemTeste = codigosPedidos[i];
+            int contagemAtualTeste = 0;
+
+            for(int j = 0; j < codigosPedidos.length; j++){
+                if (codigosPedidos[j] == itemTeste) {
+                    contagemAtualTeste++;
+                }
+            }
+
+            if (contagemAtualTeste > contagemMaisAlta) {
+                itemMaisFrequente = itemTeste;
+                contagemMaisAlta = contagemAtualTeste;
+                
+            }
+        }
+
+        System.out.println("O item " + itemMaisFrequente + " foi o mais pedido: " + contagemMaisAlta + " vezes.");
+
+
+            
+        }
+    }
+
+
+
     
-}
