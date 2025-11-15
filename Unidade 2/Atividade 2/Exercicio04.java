256a50baaa2e02fcd39cff769375b1169d67045d
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:32:15 -0300

    verificação expressão pilhas

---
+++ Unidade 2/Atividade 2/Exercicio04.java
@@ -0,0 +1,10 @@
+/*
+Verificação de Expressões: No desenvolvimento de uma calculadora, você precisa verificar se a expressão matemática digitada está com os parênteses corretamente balanceados. 
+Utilize uma pilha para desenvolver um programa que verifique essa estrutura, ajudando a identificar possíveis erros antes do cálculo. 
+*/
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        
+    }
+}
