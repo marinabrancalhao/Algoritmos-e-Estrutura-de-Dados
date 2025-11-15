6205f289cdee376b7569ee650086f9d8890e593f
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:35:10 -0300

    fila de espera

---
+++ Unidade 2/Atividade 2/Exercicio06.java
@@ -0,0 +1,11 @@
+/*
+Implementação de um Sistema de Fila de Espera: Em uma clínica, os pacientes aguardam atendimento em uma fila de espera. 
+Crie um sistema que simule essa fila, permitindo adicionar, remover e exibir a ordem de atendimento dos pacientes. 
+Isso ajudará na organização e no controle da ordem de atendimento.  
+*/
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        
+    }
+}
