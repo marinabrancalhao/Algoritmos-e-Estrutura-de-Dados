b62562c86f1f642e4beaa1324e093ab41d131bcc
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:38:37 -0300

    mesclar mapas

---
+++ Unidade 2/Atividade 2/Exercicio09.java
@@ -0,0 +1,10 @@
+/*
+Mesclar Mapas: Em um sistema de inventário, você tem dois registros de produtos e deseja mesclar as informações. 
+Implemente um método para mesclar dois mapas (HashMaps) de produtos, cuidando para tratar possíveis conflitos de chaves. 
+*/
+
+public class Exercicio09 {
+    public static void main(String[] args) {
+        
+    }
+}
