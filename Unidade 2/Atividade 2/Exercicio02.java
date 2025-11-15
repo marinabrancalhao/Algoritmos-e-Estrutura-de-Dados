51c5bc3a1c641781309cd6f75233e3dab451d3e0
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:28:55 -0300

    remoção elementos duplicados

---
+++ Unidade 2/Atividade 2/Exercicio02.java
@@ -0,0 +1,10 @@
+/*
+Remoção de Elementos Duplicados: Em um sistema de cadastro, é comum que contatos sejam adicionados mais de uma vez. 
+Crie um método que remova contatos duplicados de uma lista de usuários (ArrayList), deixando apenas uma entrada por pessoa e otimizando o banco de dados. 
+*/
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        
+    }
+}
