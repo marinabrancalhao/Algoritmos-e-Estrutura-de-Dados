1dc3bed0f6585e9214ec4997d6968ea2dbe4fe62
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:37:36 -0300

    contagem de palavras

---
+++ Unidade 2/Atividade 2/Exercicio08.java
@@ -0,0 +1,10 @@
+/*
+Contagem de Palavras: Você precisa criar uma análise de palavras usadas em um artigo para entender quais termos aparecem com maior frequência. 
+Escreva um programa que conte a frequência de cada palavra em uma frase, utilizando um HashMap, para identificar as palavras mais comuns.  
+*/
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        
+    }
+}
