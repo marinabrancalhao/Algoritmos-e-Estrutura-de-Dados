5c870293b6fa8facb2c46576e8adb5c88033c40a
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:27:09 -0300

    inversão de lista

---
+++ Unidade 2/Atividade 2/Exercicio01.java
@@ -0,0 +1,10 @@
+/*
+Inversão de Lista: Imagine que você está desenvolvendo um recurso em um aplicativo que exibe uma lista de tarefas do dia em ordem inversa.
+Escreva um programa que inverta os elementos de uma lista de tarefas (ArrayList) sem utilizar métodos prontos da linguagem, dando ao usuário a opção de ver a lista do fim para o começo. 
+*/
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        
+    }
+}
