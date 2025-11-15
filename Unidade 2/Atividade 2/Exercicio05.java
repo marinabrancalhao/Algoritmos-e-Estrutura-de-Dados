f2fc6c0e7faa2e26b9a0feb0b0550b2dfe225aad
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:33:40 -0300

    inversão com pilha

---
+++ Unidade 2/Atividade 2/Exercicio05.java
@@ -0,0 +1,10 @@
+/*
+Inversão com Pilha: Suponha que você está desenvolvendo uma ferramenta que permite ao usuário visualizar uma lista de produtos na ordem inversa da inserção original. 
+Implemente um método que utilize uma pilha para inverter a ordem dos elementos de uma lista de produtos (ArrayList)
+*/
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        
+    }
+}
