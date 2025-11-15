191ddbe0d3d60f72e3a201900dd8f1f87cb2afa5
Marina <mahbrancalhao@gmail.com>
2025-11-15 02:27:51 -0300

    inversão de lista

--- Unidade 2/Atividade 2/Exercicio01.java
+++ Unidade 2/Atividade 2/Exercicio01.java
@@ -3,8 +3,39 @@ Inversão de Lista: Imagine que você está desenvolvendo um recurso em um aplic
 Escreva um programa que inverta os elementos de uma lista de tarefas (ArrayList) sem utilizar métodos prontos da linguagem, dando ao usuário a opção de ver a lista do fim para o começo. 
 */
 
+import java.util.ArrayList;
+
 public class Exercicio01 {
-    public static void main(String[] args) {
+
+    public static void inverterLista(ArrayList<String> tarefasDoDia){
         
+        ArrayList<String> listaInvertida = new ArrayList<>();
+       
+        int tamanho = tarefasDoDia.size();
+        for(int i = tamanho - 1; i >= 0; i--){
+            listaInvertida.add(tarefasDoDia.get(i));
+        }
+
+        System.out.println("\n-- Tarefas do Dia | Ordem Decrescente--");
+        for (String tarefa : listaInvertida) {
+            System.out.println(tarefa);
+        }
+    }
+    public static void main(String[] args) {
+
+        ArrayList<String> tarefasDoDia = new ArrayList<>();
+
+        tarefasDoDia.add("8h - Ir para academia");
+        tarefasDoDia.add("11h30 - Almoço com Ana");
+        tarefasDoDia.add("15h - Reunião");
+        tarefasDoDia.add("18h30 - Aula de inglês");
+        tarefasDoDia.add("19h30 - Passar no supermercado");
+
+        System.out.println("-- Tarefas do Dia | Ordem Crescente--");
+        for (String tarefa : tarefasDoDia) {
+            System.out.println(tarefa);
+        }
+
+        inverterLista(tarefasDoDia);
     }
 }
