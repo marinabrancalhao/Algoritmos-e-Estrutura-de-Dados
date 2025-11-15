fdfdf94ba65d184ac72959574730322de10ecd3c
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:41:32 -0300

    algoritmo de ordenação

---
+++ Unidade 2/Atividade 2/Exercicio10.java
@@ -0,0 +1,10 @@
+/*
+Implementação de Algoritmo de Ordenação: Imagine que você está desenvolvendo um sistema de classificação que exibe os produtos mais populares em uma loja online. 
+Escolha um algoritmo de ordenação (ex: Bubble Sort, Quick Sort, Merge Sort) e implemente-o para ordenar uma lista de produtos com base nas vendas, de forma decrescente, para que os mais vendidos apareçam primeiro.
+*/
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+
+    }
+}
