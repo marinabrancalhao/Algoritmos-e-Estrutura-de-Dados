008f82257cd7853e14b65e3ad43aec28fd85d004
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:36:03 -0300

    ordem de chegada

---
+++ Unidade 2/Atividade 2/Exercicio07.java
@@ -0,0 +1,10 @@
+/*
+Ordem de Chegada: Imagine que você está organizando o fluxo de clientes em uma loja durante uma promoção. 
+Crie um programa que simule a ordem de chegada e exiba a ordem de saída, respeitando a fila de atendimento. 
+*/
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        
+    }
+}
