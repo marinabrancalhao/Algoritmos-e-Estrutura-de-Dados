721b46c3d83c40aa4701165157c25cecc176c87e
Marina <mahbrancalhao@gmail.com>
2025-11-15 01:30:01 -0300

    mesclar listas

---
+++ Unidade 2/Atividade 2/Exercicio03.java
@@ -0,0 +1,10 @@
+/*
+Mesclar Listas: Suponha que você está gerenciando duas listas de convidados para um evento e deseja criar uma nova lista com os nomes intercalados das listas originais. 
+Implemente uma função que receba duas listas de convidados e retorne uma nova lista com os elementos intercalados.
+*/
+
+public class Exercicio03 {
+   public static void main(String[] args) {
+    
+   } 
+}
