5b007a8bec997165a47608816b24aab9524a2a72
Marina <mahbrancalhao@gmail.com>
2025-11-17 19:06:27 -0300

    contagem de palavras

--- Unidade 2/Atividade 2/Exercicio08.java
+++ Unidade 2/Atividade 2/Exercicio08.java
@@ -3,8 +3,41 @@ Contagem de Palavras: Você precisa criar uma análise de palavras usadas em um 
 Escreva um programa que conte a frequência de cada palavra em uma frase, utilizando um HashMap, para identificar as palavras mais comuns.  
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercicio08 {
     public static void main(String[] args) {
         
+        String frase = "O tempo perguntou pro tempo quanto tempo o tempo tem";
+        
+        String[] palavras = frase.toLowerCase().split(" ");
+
+        Map<String, Integer> contador = new HashMap<>();
+
+        for (String palavra : palavras) {
+            if (contador.containsKey(palavra)) {
+                contador.put(palavra, contador.get(palavra) + 1);
+            } else {
+                contador.put(palavra, 1);
+            } 
+        }
+
+        int maiorValor = 0;
+        for (Integer valor : contador.values()) {
+            if (valor > maiorValor) {
+                maiorValor = valor;
+            }
+        }
+
+        System.out.print("Palavra mais frequente: ");
+        for (String chave : contador.keySet()) {
+            if (contador.get(chave) == maiorValor){
+                System.out.println(chave);
+            }  
+        }
+
+        System.out.println("Repete " + maiorValor + " vezes");
     }
+  
 }
