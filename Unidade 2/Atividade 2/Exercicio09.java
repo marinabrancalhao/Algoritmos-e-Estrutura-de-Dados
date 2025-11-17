0752db27fdfd744536ed4e4c9b3e52abbb6b8bde
Marina <mahbrancalhao@gmail.com>
2025-11-17 19:05:55 -0300

    mesclar mapas

--- Unidade 2/Atividade 2/Exercicio09.java
+++ Unidade 2/Atividade 2/Exercicio09.java
@@ -3,8 +3,41 @@ Mesclar Mapas: Em um sistema de inventário, você tem dois registros de produto
 Implemente um método para mesclar dois mapas (HashMaps) de produtos, cuidando para tratar possíveis conflitos de chaves. 
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercicio09 {
     public static void main(String[] args) {
+
+        Map<String, Integer> estoque1 = new HashMap<>();
+        estoque1.put("Caderno", 10);
+        estoque1.put("Lápis", 15);
+        estoque1.put("Agenda", 3);
+
+        System.out.println("Estoque 1: " + estoque1);
+
+        Map<String, Integer> estoque2 = new HashMap<>();
+        estoque2.put("Agenda", 2);
+        estoque2.put("Caneta", 12);
+        estoque2.put("Borracha", 8);
+
+        System.out.println("Estoque 2: " + estoque2);
+
+        for (String produto : estoque2.keySet()) {
+
+            int quantidadeEstoque2 = estoque2.get(produto);
+
+            if (estoque1.containsKey(produto)) {
+                int quantidadeEstoque1 = estoque1.get(produto);
+                estoque1.put(produto, quantidadeEstoque1 + quantidadeEstoque2);
+            } else {
+                estoque1.put(produto, quantidadeEstoque2);
+            }
+            
+        }
+
+        System.out.println("\nEstoque Unificado: " + estoque1);
+    
         
     }
 }
