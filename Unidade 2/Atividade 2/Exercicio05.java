82c062702a3e533e309fb78f8a36c18939da22c6
Marina <mahbrancalhao@gmail.com>
2025-11-17 15:43:25 -0300

    inversão com pilha

--- Unidade 2/Atividade 2/Exercicio05.java
+++ Unidade 2/Atividade 2/Exercicio05.java
@@ -3,8 +3,46 @@ Inversão com Pilha: Suponha que você está desenvolvendo uma ferramenta que pe
 Implemente um método que utilize uma pilha para inverter a ordem dos elementos de uma lista de produtos (ArrayList)
 */
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Exercicio05 {
+
+    public static void inverterLista(ArrayList<String> produtos){
+
+        Stack<String> pilha = new Stack<>();
+
+        for (String produto : produtos) {
+            pilha.push(produto);
+        }
+
+        produtos.clear();
+
+        while (!pilha.isEmpty()) {
+            produtos.add(pilha.pop()); 
+        }
+
+    }
     public static void main(String[] args) {
-        
+
+        ArrayList<String> produtos = new ArrayList<>();
+
+        produtos.add("Mouse");
+        produtos.add("Teclado");
+        produtos.add("Monitor");
+        produtos.add("Impressora");
+        produtos.add("Webcam");
+
+        System.out.println("-- Lista de produtos --");
+        for (String produto : produtos) {
+            System.out.println(produto);
+        }
+
+        inverterLista(produtos);
+
+        System.out.println("\n-- Lista de produtos invertida --");
+        for (String produto : produtos) {
+            System.out.println(produto);
+        }  
     }
 }
