9528d26ec505c1adac17f5c1164ea243c2def4c3
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:20:10 -0300

    representação de grafos

---
+++ Unidade 3/Atividade 3/Exercicio05.java
@@ -0,0 +1,13 @@
+/*
+Representação de Grafos:
+Você está desenvolvendo um sistema de roteamento para um aplicativo de navegação.
+Crie uma classe que represente um grafo em Java, utilizando lista de adjacências ou matriz de adjacências, para mapear as conexões entre diferentes locais. 
+*/
+
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
