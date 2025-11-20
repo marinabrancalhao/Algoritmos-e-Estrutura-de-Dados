a58b61a375390eba06c57a6f3f0867fe5b254745
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:26:04 -0300

    BFS em grafos

---
+++ Unidade 3/Atividade 3/Exercicio06.java
@@ -0,0 +1,11 @@
+/*
+BFS em Grafos:
+No contexto do seu aplicativo de navegação, implemente o algoritmo de Busca em Largura (BFS) para encontrar o caminho mais curto entre dois pontos em um grafo não ponderado, ajudando os usuários a escolherem a melhor rota. 
+*/
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
