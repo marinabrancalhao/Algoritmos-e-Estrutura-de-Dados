1d3670e6839771d70c7549e37f56abc0aace8c65
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:29:56 -0300

    grafo ponderado

---
+++ Unidade 3/Atividade 3/Exercicio09.java
@@ -0,0 +1,13 @@
+/*
+Caminho Mais Curto em Grafo Ponderado:
+Ao modificar seu algoritmo BFS, você precisa encontrar o caminho mais curto entre dois pontos em um grafo ponderado, considerando as distâncias. 
+Esse ajuste é crucial para garantir que os usuários recebam as rotas mais eficientes. 
+*/
+
+public class Exercicio09 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
