614190e896f03fcac64f67b1066bfc7dbaeca364
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:18:27 -0300

    busca binária

---
+++ Unidade 3/Atividade 3/Exercicio04.java
@@ -0,0 +1,13 @@
+/*
+Busca Binária:
+Imagine que você está desenvolvendo um sistema de pesquisa de dados em uma lista de números ordenados.
+Implemente um algoritmo de busca binária que encontre a posição de um elemento específico em um array ordenado de inteiros, retornando sua posição ou -1 se não for encontrado. 
+*/
+
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
