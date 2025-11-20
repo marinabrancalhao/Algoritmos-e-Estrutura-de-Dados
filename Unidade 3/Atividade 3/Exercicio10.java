73424b264f3962d3f2ce5dfca583f5238ccb0fc5
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:31:01 -0300

    ordenação de grafos

---
+++ Unidade 3/Atividade 3/Exercicio10.java
@@ -0,0 +1,13 @@
+/*
+Ordenação de Grafos:
+Por fim, implemente um algoritmo que ordene os vértices de um grafo de acordo com a topologia das conexões entre eles. 
+Essa ordenação pode ser útil em diversas aplicações, como planejamento de projetos ou organização de tarefas interdependentes. 
+*/
+
+public class Exercicio10 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
