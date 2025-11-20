95709bf6d6079d896e967780d097aea471045fdd
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:15:27 -0300

    validação BST

---
+++ Unidade 3/Atividade 3/Exercicio02.java
@@ -0,0 +1,13 @@
+/*
+Validação de BST:
+Suponha que você tenha uma árvore que armazena a hierarquia de categorias de produtos em um e-commerce.
+Escreva um método que verifique se essa árvore binária é uma árvore binária de busca (BST), garantindo que os produtos estejam organizados corretamente para facilitar a busca e navegação. 
+*/
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
