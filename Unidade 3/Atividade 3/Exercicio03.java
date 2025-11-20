e4295838521751ed2c4fbf1992dffc62c2e35c45
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:16:56 -0300

    busca linear

---
+++ Unidade 3/Atividade 3/Exercicio03.java
@@ -0,0 +1,12 @@
+/*
+Busca Linear:
+Você está criando um aplicativo de agenda de contatos e precisa encontrar rapidamente a posição de um número específico na lista.
+Escreva um método que realize uma busca linear em um array de inteiros, retornando a posição do número buscado ou -1 caso ele não esteja presente. 
+*/
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        
+    }
+}
