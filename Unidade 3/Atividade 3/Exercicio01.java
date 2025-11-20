50567dc3aed5548163a7de554b6daa10710c439d
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:13:48 -0300

    implementação árvore binária

---
+++ Unidade 3/Atividade 3/Exercicio01.java
@@ -0,0 +1,13 @@
+/*Implementação da Árvore Binária:
+Imagine que você está desenvolvendo um sistema de gerenciamento de bibliotecas.
+Crie uma classe em Java para representar uma árvore binária, onde cada nó armazenará informações sobre um livro.
+Implemente métodos que permitam inserir novos livros e percorrer a árvore em pré-ordem, pós-ordem e em ordem para listar todos os livros disponíveis. 
+*/
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
