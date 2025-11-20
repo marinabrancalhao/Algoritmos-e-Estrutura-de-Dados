1b4d6942be38e9c64dfc54cb1042896538bf5135
Marina <mahbrancalhao@gmail.com>
2025-11-20 00:23:36 -0300

    DFS em grafos

---
+++ Unidade 3/Atividade 3/Exercicio08.java
@@ -0,0 +1,13 @@
+/*
+DFS em Grafos:
+Continuando no seu aplicativo de navegação, escreva um método para realizar a Busca em Profundidade (DFS) em um grafo.
+Esse método deve exibir todos os vértices visitados, permitindo que os usuários visualizem as possíveis rotas de maneira mais detalhada. 
+*/
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
